42f65bba803d3eaa509fe527f40e435deef9b0e9
RnDQtera <bagus@qtera.co.id>
2024-09-06 11:19:57 +0700

    Borrow transaction

---
+++ Object Repository/OBJ.016 - Fixed Asset - Borrow Transaction/OBJ.016.001 Action Borrow.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OBJ.016.001 Action Borrow</name>
+   <tag></tag>
+   <elementGuidId>59e2fddc-e367-4fdd-930d-f4674884c53d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[7]/div/div[3]/div/div/button[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
