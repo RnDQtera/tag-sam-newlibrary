de6b27e3722d572007c5935443468d57388a2116
KrisnaTriAnggara <110578223+KrisnaTriAnggara@users.noreply.github.com>
2024-09-26 17:27:23 +0700

    Udpate

---
+++ Object Repository/OBJ Modul License/All Licenses Tab/Dropdown Duration Expired Tab.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Dropdown Duration Expired Tab</name>
+   <tag></tag>
+   <elementGuidId>6152e2fc-c101-4fb4-a535-c72c525dde43</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[1]/main/div/div/div[2]/div/div/div[7]/div[4]/div/div/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/OBJ Modul License/All Licenses Tab/List Field (1) Expired Tab.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>List Field (1) Expired Tab</name>
+   <tag></tag>
+   <elementGuidId>236e09ad-1c72-4dda-8027-bf88a63e40b9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[7]/div[2]/ul/li[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
